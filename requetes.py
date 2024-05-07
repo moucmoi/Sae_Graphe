e757e859bd8d4956ddd8cc38e02e7931b4b8380c
moucmoi <143808893+moucmoi@users.noreply.github.com>
2024-05-07 14:40:09 +0200

    creation fichier requetes (#2)
    * creation fichier requetes
    
    * creation requetes et imports
    
    * fonctions
    
    * fonctions

---
+++ requetes.py
@@ -0,0 +1,24 @@
+import json
+import networkx as nx
+
+# Q1
+def json_vers_nx(chemin):
+
+# Q2
+def collaborateurs_communs(G,u,v):
+
+# Q3
+def collaborateurs_proches(G,u,k):
+def est_proche(G,u,v,k=1):
+def distance_naive(G,u,v):
+def distance(G,u,v):
+
+# Q4
+def centralite(G,u):
+def centre_hollywood(G):
+
+# Q5
+def eloignement_max(G:nx.Graph):
+
+# Bonus
+def centralite_groupe(G,S):

ef9768d0ee4f9b343a5a090b1abb21586e689b1a
Noa Fonteny <noatoimoi@gmail.com>
2024-05-07 14:38:23 +0200

    fonctions

--- requetes.py
+++ requetes.py
@@ -1,2 +1,25 @@
 import json
 import networkx as nx
+
+# Q1
+def json_vers_nx(chemin):
+
+# Q2
+def collaborateurs_communs(G,u,v):
+
+# Q3
+def collaborateurs_proches(G,u,k):
+def est_proche(G,u,v,k=1):
+def distance_naive(G,u,v):
+def distance(G,u,v):
+
+# Q4
+def centralite(G,u):
+def centre_hollywood(G):
+
+# Q5
+5
+def eloignement_max(G:nx.Graph):
+
+# Bonus
+def centralite_groupe(G,S):
